ffef290d3daece15560c2f49dd7b78faf2382354
Alex Miller <alexannmill@gmail.com>
2022-10-20 12:39:53 -0700

    page complete wih some style can use more if time allows

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,12 @@
 class Admin::DashboardController < ApplicationController
 
-  # http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD'], except: [:index]
-
+  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD'], except: [:index]
 
   def show
+    @products = Product.all.order(created_at: :desc)
+    @categories = Category.all.order(created_at: :desc)
+    @productcount = @products.count
+    @categoriescount = @categories.count
   end
+
 end
